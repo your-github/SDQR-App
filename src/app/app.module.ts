517647d9d12a6da1bdf14826cf747f185bcb8571
Youathor Yiaveu <yourthor17121996@gmail.com>
2017-07-01 11:27:53 +0700

    add qrcode scanner and configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+/*** IONIC native plugin */
+import {BarcodeScanner} from '@ionic-native/barcode-scanner';
+
+/*** Firebase module */
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDKfhalS9iXAH1Lqr_Z7HcoYF0mHHmHfd0',
+    authDomain: 'dbook-8d9fa.firebaseapp.com',
+    databaseURL: 'https://dbook-8d9fa.firebaseio.com',
+    projectId: 'dbook-8d9fa',
+    storageBucket: 'dbook-8d9fa.appspot.com',
+    messagingSenderId: '951635947188'
+}
 
 @NgModule({
   declarations: [
@@ -18,6 +35,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +48,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner
   ]
 })
 export class AppModule {}
